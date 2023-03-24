abce8588a4cb7097fcfacc40d4f54a13c739b9de
vasa007 <56633518+vasa007@users.noreply.github.com>
2023-03-24 10:55:33 +0300

    Add files via upload

--- 2023-02-08-circles/intersection.py
+++ 2023-02-08-circles/intersection.py
@@ -19,27 +19,46 @@ class Intersection(Shape):
             self.intersectLL(self.sh1, self.sh2)
         if type(self.sh1) is Circle and type(self.sh2) is Line:
             self.intersectCL(self.sh1, self.sh2)
+        if type(self.sh1) is Circle and type(self.sh2) is Circle:
+            self.intersectCC(self.sh1, self.sh2)
         for p in self._points:
             p.redraw()
     
     def intersectCL(self, circle1, line1):
         self._points = []
-        center = circle1.C
-        P = circle1.P
-        A = line1.p0
-        B = line1.p1
-        n = (A - B).left()
-        n = n.Dir()
-        u = n.right()
-        u = u.Dir()
-        rad = Vec.len1(center - P)
-        x = (A - center) * n
-        if rad ** 2 - x ** 2 >= 0:
-            y = (rad ** 2 - x ** 2) ** 0.5
-            self._points.append(Point(self._c, (center.x + x + y, center.y + y), "blue"))
-            self._points.append(Point(self._c, (center.x - x, center.y - y), "blue"))
-            self._points.append(Point(self._c, (center.x + x, center.y - y), "blue"))
-            self._points.append(Point(self._c, (center.x - x, center.y + y), "blue"))
+        p = circle1.C
+        
+        u = (line1.p0 - line1.p1).left()
+        v = u.left()
+        t2, t1 = (p - line1.p0).to_base(u, -v)
+        d = u * t1
+        R = (circle1.C - circle1.P).len2()
+        if R - d.len2() >= 0:
+            dlin = (R - d.len2()) ** 0.5
+            b = ((line1.p1 - line1.p0).Dir()) * dlin
+            
+            point1 = b + p - d
+            point2 = p - d - b
+            self._points.append(Point(self._c, (point1.x, point1.y), "blue"))
+            self._points.append(Point(self._c, (point2.x, point2.y), "blue"))
+        else:
+            pass
+
+    def intersectCC(self, circle1, circle2):
+        self._points = []
+        R1 = (circle1.C - circle1.P).len2()
+        R2 = (circle2.C - circle2.P).len2()
+        d = (circle2.C - circle1.C).len1()
+        a = (R1 - R2 + d**2) / (2 * d)
+        t = circle1.C + (circle2.C - circle1.C) * (a / d)
+        if (R1 - a ** 2) >= 0:
+            h = (R1 - a ** 2) ** 0.5
+            point1 = Vec(t.x + (circle2.C.y - circle1.C.y) * (h / d), t.y - (circle2.C.x - circle1.C.x) * (h / d))
+            point2 = Vec(t.x - (circle2.C.y - circle1.C.y) * (h / d), t.y + (circle2.C.x - circle1.C.x) * (h / d))
+            self._points.append(Point(self._c, (point1.x, point1.y), "blue"))
+            self._points.append(Point(self._c, (point2.x, point2.y), "blue"))
+        else:
+            pass
 
     def intersectLL(self, line1, line2):
         self._points = []
